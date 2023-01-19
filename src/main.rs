291b776921629391c3cf723ecae817afc4288717
“Rustlanachain” <hamza.sajid@invozone.com>
2023-01-19 23:45:28 +0500

    new input output

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+fn main() -> io::Result<()> {
+    let mut user_input = String::new();
+    let stdin = io::stdin(); // We get `Stdin` here.
+    stdin.read_line(&mut user_input);
+    println!("Hello, World.");
+    println!("{} ", user_input);
+
+    Ok(())
 }
